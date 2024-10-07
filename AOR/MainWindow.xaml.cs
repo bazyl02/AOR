3438e554c5f697f90fe43c448253fae895986c1b
Mateusz Podgórski <bazyl0022@gmail.com>
2024-10-07 20:44:31 +0200

    Some testing with MIDI library

--- AOR/MainWindow.xaml.cs
+++ AOR/MainWindow.xaml.cs
@@ -1,13 +1,50 @@
-﻿namespace AOR
+﻿using System;
+using System.Linq;
+using System.Windows;
+using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Multimedia;
+
+namespace AOR
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
+        private static IInputDevice device;
         public MainWindow()
         {
             InitializeComponent();
+            
+            
+
+            /*
+            var outputDevice = OutputDevice.GetByIndex(0);
+            outputDevice.SendEvent(new NoteOnEvent((SevenBitNumber)60, (SevenBitNumber)45));
+            outputDevice.SendEvent(new NoteOffEvent((SevenBitNumber)60, (SevenBitNumber)0)
+            {
+                DeltaTime = 400
+            });
+            */
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            device.Dispose();
+            base.OnClosed(e);
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            device = InputDevice.GetByName("CASIO USB-MIDI");
+            device.EventReceived += OnEventReceived;
+            device.StartEventsListening();
+            Text1.Text = device.IsListeningForEvents.ToString();
+        }
+        
+        private void OnEventReceived(object sender, MidiEventReceivedEventArgs e)
+        {
+            var midiDevice = (MidiDevice)sender;
+            Text1.Text = "kek";
+            Text1.Text = "Event received from " + midiDevice.Name + " at " + DateTime.Now + ": " + e.Event;
         }
     }
 }
