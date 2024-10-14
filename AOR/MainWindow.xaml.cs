10dd795464d673c05f15393d9cc1478c84519d1c
242500 <126494791+bazyl02@users.noreply.github.com>
2024-10-14 16:35:06 +0200

    Testing MIDI file reading

--- AOR/MainWindow.xaml.cs
+++ AOR/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using AOR.ModelView;
+﻿using System;
+using System.IO;
+using System.Windows;
+using AOR.ModelView;
 using AOR.Model;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Multimedia;
 
 namespace AOR
 {
@@ -8,6 +13,8 @@ namespace AOR
         private DeviceController DeviceController;
 
         private Bindings _bindings;
+
+        private static Playback _playback;
         
         public MainWindow()
         {
@@ -15,6 +22,30 @@ namespace AOR
             _bindings = Bindings.GetInstance();
             DataContext = _bindings;
             _bindings.DeviceController = new DeviceController();
+            StreamWriter raportFile = new StreamWriter("../../Data/Raport1.txt");
+            MidiFile file = MidiFile.Read("../../Data/Megalovania.mid");
+            var chunks = file.Chunks;
+            Console.WriteLine(chunks.Count);
+            foreach (TrackChunk chunk in chunks)
+            {
+                raportFile.WriteLine("Chunk size: " + chunk.Events.Count);
+                raportFile.WriteLine("Events: ");
+                foreach (MidiEvent event1 in chunk.Events)
+                {
+                    raportFile.WriteLine(event1.ToString() + " | Time: " + event1.DeltaTime);
+                }
+                raportFile.WriteLine("------------------------------------------------");
+            }
+            raportFile.Close();
+            
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            MidiFile file = MidiFile.Read("../../Data/Megalovania.mid");
+            _playback = file.GetPlayback(Bindings.GetInstance().DeviceController.OutputDevice);
+            _playback.Start();
+            
         }
     }
 }
