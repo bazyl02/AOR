cc96bd808077f621ba98cd3f39888fd007214d47
Mateusz Podgórski <bazyl0022@gmail.com>
2024-10-26 19:05:09 +0200

    Testing some MIDI interactions

--- AOR/Model/PieceBuffer.cs
+++ AOR/Model/PieceBuffer.cs
@@ -11,10 +11,26 @@ namespace AOR.Model
         public PieceBuffer(MidiFile file)
         {
             var chunks = file.Chunks;
-            
             foreach (var chunk in chunks)
             {
-                Console.WriteLine(chunk.ChunkId);
+                if (chunk.ChunkId == "MTrk")
+                {
+                    TrackChunk trackChunk = (TrackChunk)chunk;
+                    var events = trackChunk.Events;
+                    var channels = trackChunk.GetChannels();
+                    foreach (var channel in channels)
+                    {
+                        Console.WriteLine(channel);
+                    }
+                    foreach (var ev in events)
+                    {
+                        //Console.WriteLine(ev.EventType);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(@"Header chunk detected!");
+                }
             }
         }
     }
