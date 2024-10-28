e0ec34d2c36cf80964452d838fac123ce2e9905a
Mateusz Podgórski <bazyl0022@gmail.com>
2024-10-28 17:16:58 +0100

    Some tyding

--- AOR/SheetWindow.xaml.cs
+++ AOR/SheetWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,47 +17,36 @@ namespace AOR
         {
             InitializeComponent();
             DataContext = Bindings.GetInstance();
-            Uri uri = new Uri("F:\\Pulpit/XDDD.png");
-            Bindings.GetInstance().CurrentSheet = new BitmapImage(uri);
         }
 
         public async Task<List<BitmapImage>> GetPdfPages(string path)
         {
             List<BitmapImage> output = new List<BitmapImage>();
-            var storagePdfFile = await StorageFile.GetFileFromPathAsync(path);
+            StorageFile storagePdfFile = await StorageFile.GetFileFromPathAsync(path);
             PdfDocument pdfDocument = await PdfDocument.LoadFromFileAsync(storagePdfFile);
-            
             for (uint i = 0; i < pdfDocument.PageCount; i++)
             {
                 using (PdfPage page = pdfDocument.GetPage(i))
                 {
                     using (InMemoryRandomAccessStream memStream = new InMemoryRandomAccessStream())
                     {
-                        PdfPageRenderOptions renderOptions = new PdfPageRenderOptions();
                         await page.RenderToStreamAsync(memStream);
-                        using (Image image = Image.FromStream(memStream.AsStream()))
-                        {
-                            //Bitmap bmp = new Bitmap(image);
-                            var bi = new BitmapImage(); 
-                            bi.BeginInit();
-                            bi.CacheOption = BitmapCacheOption.OnLoad;
-                            bi.StreamSource = memStream.AsStream();
-                            bi.EndInit();
-                            
-                            output.Add(bi);
-                        }
+                        var bi = new BitmapImage(); 
+                        bi.BeginInit();
+                        bi.CacheOption = BitmapCacheOption.OnLoad;
+                        bi.StreamSource = memStream.AsStream();
+                        bi.EndInit();
+                        output.Add(bi);
                     }
                 }
             }
-            
-
             return output;
-            
         }
 
         private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            List<BitmapImage> pages = await GetPdfPages("F:\\Pulpit\\Sprawozdanie2.pdf");
+            string path = Path.GetFullPath("..\\..\\Data\\Sprawozdanie2.pdf");
+            List<BitmapImage> pages = await GetPdfPages(path);
             Bindings.GetInstance().CurrentSheet = pages[1];
         }
     }
