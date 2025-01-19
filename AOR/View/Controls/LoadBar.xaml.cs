ab38b4489495ed65e91faf01cb128b4a56ab441f
Mateusz Podgórski <bazyl0022@gmail.com>
2025-01-19 15:48:03 +0100

    Light changes

---
+++ AOR/View/Controls/LoadBar.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AOR.View.Controls
+{
+    public partial class LoadBar : UserControl
+    {
+        public LoadBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
